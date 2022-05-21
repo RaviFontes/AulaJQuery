185c20052a48c48438dc3f98642ab0ae0332f258
Ravi Fontes <ravi.souza@gmail.com>
2022-05-21 16:34:30 -0300

    Aula de Jquery 21.05.2022

---
+++ javascript/2.js
@@ -0,0 +1,7 @@
+$('#div1').text('Ola! Bem vindo ');
+
+/*JQuery Text() Method
+
+Os text() método define ou retornao conteudo de texto dos elementos selecionados.Quando este
+
+*/ 
---
+++ javascript/4.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#texto1").css("color","red");
+    $("#texto2").css("font-size","32px");
+    $("#texto2").css("background","Green");
+    $("#texto3").css("border-color","black");
+});
